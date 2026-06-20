44e53eb5c391a468c935d68957ed635c787010e1
FRANCISCO LUIZ VIANA <37913286+luizviana38@users.noreply.github.com>
2026-06-20 18:03:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -12,18 +12,22 @@ app.use(express.static(__dirname));
 
 const caminhoDados = path.join(__dirname, 'dados_bolao.json');
 
-// Rota para ler os dados salvos
+// Rota para ler os dados salvos (GET)
 app.get('/api/dados', (req, res) => {
     if (!fs.existsSync(caminhoDados)) {
         return res.json({ resultadosOficiais: {}, palpites: {} });
     }
     fs.readFile(caminhoDados, 'utf8', (err, data) => {
         if (err) return res.status(500).send("Erro ao ler dados.");
-        res.json(JSON.parse(data || '{}'));
+        try {
+            res.json(JSON.parse(data || '{}'));
+        } catch (e) {
+            res.json({ resultadosOficiais: {}, palpites: {} });
+        }
     });
 });
 
-// Rota para salvar os dados recebidos do index.html
+// Rota para salvar os dados recebidos do index.html (POST)
 app.post('/api/salvar', (req, res) => {
     const novosDados = req.body;
     fs.writeFile(caminhoDados, JSON.stringify(novosDados, null, 2), 'utf8', (err) => {
